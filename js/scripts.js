b0d60849935f81932096d531c80776af56c08e6d
Pietro Giordano <giopi87@gmail.com>
2023-01-09 13:14:06 +0100

    fine esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,27 @@ Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che 
 Consigli:
 - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+1. creare lista della spesa
+2. prendere valori uno alla volta e stamparli
 */
+const shopList = [
+      'carne',
+      'uova',
+      'latte',
+      'frutta',
+      'verdura',
+      'fazzoletti',
+      'carta igienica'
+]
+
+let i = 0;
+while (i < shopList.length) {
+
+      const newLi = document.createElement('li');
+      newLi.innerHTML = shopList[i];
+      document.querySelector('ul').append(newLi);
+
+      i++;
+
+}
